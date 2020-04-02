9b0cbac3f0db9fb9ea3b725fa6b2b6596a6621ab
Isaac de Moraes <30375595+Iakim@users.noreply.github.com>
2020-04-02 09:51:19 -0300

    Create service_and_daemon.sh

---
+++ service_and_daemon.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+ngx=`systemctl status nginx | grep running | wc -l`
+dae=`ps aux | grep ldap-auth | wc -l`
+
+if [ $ngx -gt 0 ]
+then
+	if [ $dae -lt 2 ]
+	then
+		rm -rf /etc/nginx/conf.d/nginx-ldap-auth-daemon.pid
+		sh /etc/nginx/conf.d/nginx-ldap-auth-daemon-ctl-rh.sh start
+		exit 0
+	else
+		exit 0
+	fi
+else
+	test=`nginx -t -q 2> nginx_result; cat nginx_result | wc -l`
+	if [ $test = 0 ]
+	then
+		fuser -k 80/tcp
+		fuser -k 80/tcp
+		service nginx start 2> /dev/null
+		rm -rf nginx_result
+		if [ $dae -lt 2 ]
+        	then
+                	rm -rf /etc/nginx/conf.d/nginx-ldap-auth-daemon.pid
+	                sh /etc/nginx/conf.d/nginx-ldap-auth-daemon-ctl-rh.sh start
+        	        exit 0
+	        else
+        	        exit 0
+	        fi
+	else
+		exit 0
+	fi
+fi
